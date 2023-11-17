9fc30fefbe5954fd1ad69e45dba9f48dd635a1ac
Waqas-13 <waqas23925@gmail.com>
2023-11-18 01:26:12 +0500

    reactPracts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Chia from "./Chia";
+
+function App() {
+  return (
+    <>
+    <h1> chai with React</h1>
+    <Chia/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Chia.js
@@ -0,0 +1,7 @@
+
+function Chia(){
+    return(
+        <h2>Chia is ready now</h2>
+    )
+}
+export default Chia
